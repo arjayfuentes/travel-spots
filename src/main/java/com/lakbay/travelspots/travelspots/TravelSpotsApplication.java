047b32d1feb05721f8043a7bdcdc18f0e5b92cbe
Arjay Fuentes <arjay630@gmail.com>
2021-04-19 07:04:07 +0800

    initial commit

---
+++ src/main/java/com/lakbay/travelspots/travelspots/TravelSpotsApplication.java
@@ -0,0 +1,13 @@
+package com.lakbay.travelspots.travelspots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSpotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelSpotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lakbay/travelspots/travelspots/TravelSpotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakbay.travelspots.travelspots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelSpotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
